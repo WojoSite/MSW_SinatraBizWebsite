b2dcf17e4d5091fc4b196ce81e468d554e67670f
Wojo <matt.wojtkun@gmail.com>
2017-06-21 14:11:28 -0400

    Initial Commit

---
+++ app.rb
@@ -0,0 +1,54 @@
+# ======= requires =======
+require "sinatra"
+require 'sinatra/activerecord'
+require "sinatra/reloader" if development?
+require 'rainbow'
+require 'geokit'
+
+# ======= database =======
+# set :database, "sqlite3:portfolio.db"  # sets the db name (will be created if it does not exist)
+def db_maker
+my_db = []
+my_lorem = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+formatted_lorem = my_lorem.gsub(/[.,]/,"").split
+  (0..4).each do |i|
+    user_data = {}
+    counter = i * 5
+    user_data[:id] = i+1  # {:id=>1}
+    user_data[:fname] = formatted_lorem[counter]
+    user_data[:lname] = formatted_lorem[counter+1]
+    user_data[:uname] = formatted_lorem[counter+2]
+    user_data[:password] = formatted_lorem[counter+3]
+    user_data[:utype] = formatted_lorem[counter+4]
+    my_db << user_data
+    user_data = {}
+  end
+puts "\n*** my database ***"
+puts my_db
+end
+# ======= home =======
+get '/' do
+  puts "\n******* home *******"
+  erb :home
+end
+
+get '/home' do
+  puts "\n******* home *******"
+  erb :home
+end
+
+# ======= login =======
+get '/signupform' do
+  puts "\n******* login *******"
+  erb :signupform
+end
+
+# ======= login =======
+post '/signup' do
+  puts "\n******* signup *******"
+  @firstname = params[:firstname]
+  @lastname = params[:lastname]
+  @email = params[:email]
+  puts params.inspect
+  erb :profile # this will show after submit happens (make an erb profile page and isert it here)
+end
