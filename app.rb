cea72ade01f4de832d454de5d1a9d41d51a26b2d
Wojo <matt.wojtkun@gmail.com>
2017-06-26 12:53:06 -0400

    initial commit

--- app.rb
+++ app.rb
@@ -2,26 +2,10 @@
 require "sinatra"
 require 'sinatra/activerecord'
 require "sinatra/reloader" if development?
-require 'rainbow'
-require 'geokit'
 
-# ======= rainbow =======
-puts "Who was the first president of the united states?"
-answer = gets.chomp.downcase
-if answer == "gw"
-    puts Rainbow('That is the correct answer').yellow
-else
-    puts Rainbow('That is the wrong answer').red
-end
-
-# ======= geokit =======
-address = Geokit::Geocoders::GoogleGeocoder.geocode '2101 New Hampshire Ave NW, Washington, DC'
-puts "\naddress:\n#{address}"
-address = Geokit::Geocoders::GoogleGeocoder.geocode '1 Windsor St, Andover, MA'
-puts "\naddress:\n#{address}"
 
 # ======= database =======
-set :database, "sqlite3:portfolio.db"  # sets the db name (will be created if it does not exist)
+# set :database, "sqlite3:portfolio.db"  # sets the db name (will be created if it does not exist)
 # ======= home =======
 get '/' do
   puts "\n******* home *******"
@@ -33,18 +17,17 @@ get '/home' do
   erb :home
 end
 
-# ======= login =======
-get '/signupform' do
-  puts "\n******* login *******"
-  erb :signupform
+get '/about' do
+  puts "\n******* home *******"
+  erb :about
 end
 
-# ======= login =======
-post '/signup' do
-  puts "\n******* signup *******"
-  @firstname = params[:firstname]
-  @lastname = params[:lastname]
-  @email = params[:email]
-  puts params.inspect
-  erb :profile # this will show after submit happens (make an erb profile page and isert it here)
+get '/gallery' do
+  puts "\n******* home *******"
+  erb :gallery
+end
+
+get '/contact' do
+  puts "\n******* home *******"
+  erb :contact
 end
